4aa60805ce3e5f8f818bd3a6b9fde60aabeb46e2
rahul sinha <rahul7909027305@gmail.com>
2025-07-11 20:17:12 +0530

    sec commit

--- src/main/java/com/example/testing/controller/TestingClass.java
+++ src/main/java/com/example/testing/controller/TestingClass.java
@@ -1,10 +1,12 @@
 package com.example.testing.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestingClass {
 	
+	@GetMapping("/")
 	public String devtestring() {
 		return "this is new pattern of our website hosting our technology is help to someone";
 	}
