cf2f8efb119d1ef4a055513d1861b411d5399dca
rahul sinha <rahul7909027305@gmail.com>
2025-07-11 20:06:11 +0530

    first commit

---
+++ src/main/java/com/example/testing/controller/TestingClass.java
@@ -0,0 +1,12 @@
+package com.example.testing.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestingClass {
+	
+	public String devtestring() {
+		return "this is new pattern of our website hosting our technology is help to someone";
+	}
+
+}
